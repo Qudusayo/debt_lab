4d3db8e17b46132e20fed64e860d6c8fb785571a
Qudusayo <zeekimono@gmail.com>
2020-10-29 23:24:14 +0100

    Completed Transactions test

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from "react-router-dom";
 import Navbar from "./Component/Navbar";
 import Home from "./Routes/Home/Home";
 import Form from './Routes/Form/Form'
+import Transaction from './Routes/Transactions/Transaction'
 
 class App extends Component {
     render() {
@@ -12,6 +13,7 @@ class App extends Component {
                 <Navbar />
                 <Route path="/" exact component={Home} />
                 <Route path="/form" exact component={Form} />
+                <Route path="/transactions" exact component={Transaction} />
             </>
         );
     }
---
+++ src/Routes/Transactions/Transaction.js
@@ -0,0 +1,116 @@
+import React, { Component } from "react";
+
+import styles from "./style.module.scss";
+
+class Transaction extends Component {
+    render() {
+        return (
+            <div className={styles.Transaction}>
+                <ul>
+                <li>
+                        <div>
+                            <h3>07016412041</h3>
+                            <span><small>STARTIMES 1M SUBSCRIPTION</small></span>
+                        </div>
+                        <div>
+                            <h3 className={styles.debit} >2400.00</h3>
+                            <span><small>22/10/2020</small></span>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <h3>07016412041</h3>
+                            <span><small>STARTIMES 1M SUBSCRIPTION</small></span>
+                        </div>
+                        <div>
+                            <h3 className={styles.debit} >200.00</h3>
+                            <span><small>22/10/2020</small></span>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <h3>07016412041</h3>
+                            <span><small>STARTIMES 1M SUBSCRIPTION</small></span>
+                        </div>
+                        <div>
+                            <h3 className={styles.debt} >350.00</h3>
+                            <span><small>22/10/2020</small></span>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <h3>07016412041</h3>
+                            <span><small>STARTIMES 1M SUBSCRIPTION</small></span>
+                        </div>
+                        <div>
+                            <h3 className={styles.debt} >2400.00</h3>
+                            <span><small>22/10/2020</small></span>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <h3>07016412041</h3>
+                            <span><small>STARTIMES 1M SUBSCRIPTION</small></span>
+                        </div>
+                        <div>
+                            <h3 className={styles.debt} >2400.00</h3>
+                            <span><small>22/10/2020</small></span>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <h3>07016412041</h3>
+                            <span><small>STARTIMES 1M SUBSCRIPTION</small></span>
+                        </div>
+                        <div>
+                            <h3 className={styles.debit} >2400.00</h3>
+                            <span><small>22/10/2020</small></span>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <h3>07016412041</h3>
+                            <span><small>STARTIMES 1M SUBSCRIPTION</small></span>
+                        </div>
+                        <div>
+                            <h3 className={styles.debt} >2400.00</h3>
+                            <span><small>22/10/2020</small></span>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <h3>07016412041</h3>
+                            <span><small>STARTIMES 1M SUBSCRIPTION</small></span>
+                        </div>
+                        <div>
+                            <h3 className={styles.debit} >2400.00</h3>
+                            <span><small>22/10/2020</small></span>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <h3>07016412041</h3>
+                            <span><small>STARTIMES 1M SUBSCRIPTION</small></span>
+                        </div>
+                        <div>
+                            <h3 className={styles.debt} >2400.00</h3>
+                            <span><small>22/10/2020</small></span>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <h3>07016412041</h3>
+                            <span><small>STARTIMES 1M SUBSCRIPTION</small></span>
+                        </div>
+                        <div>
+                            <h3 className={styles.debit} >2400.00</h3>
+                            <span><small>22/10/2020</small></span>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default Transaction;
